53881d1d042b140aee6bef4642f28efc4b54f158
Roman Saitov <strathisla.72@gmail.com>
2024-08-05 04:08:22 +0500

    Add brain-progression.js brain-progression-game.js

---
+++ src/games/brain-progression-game.js
@@ -0,0 +1,33 @@
+import askUserName from '../cli.js';
+import {
+  isAnswerCorrect, getRandomInt, maxRoundCount, userAnswer,
+} from '../index.js';
+
+export default () => {
+  const userName = askUserName();
+  console.log('What number is missing in the progression?');
+
+  for (let i = 0; i < maxRoundCount(); i += 1) {
+    const progressionFirstItem = getRandomInt(0, 20);
+    const progressionLength = getRandomInt(5, 10);
+    const progressionStep = getRandomInt(2, 7);
+    const hiddenPosition = getRandomInt(0, progressionLength - 1);
+    let correctAnswer;
+    let result = '';
+    let lastItem = progressionFirstItem;
+    for (let j = 0; j < progressionLength; j += 1) {
+      if (j === hiddenPosition) {
+        result = `${result} ${'..'}`;
+        correctAnswer = lastItem;
+        lastItem += progressionStep;
+      } result = `${result} ${lastItem}`;
+      lastItem += progressionStep;
+    }
+    const answer = userAnswer(result);
+
+    if (!isAnswerCorrect(answer, correctAnswer, userName)) {
+      return;
+    }
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
